0e41739a81a70ef786d2627603ba5b21a88b548b
Adem <ademaldemir@hotmail.com.tr>
2022-07-08 22:23:54 +0300

    KDV Tutarı Hesaplayıcı Program.

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int mat, fizik, kimya, turkce, tarih, muzik;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen Fiyat Giriniz:");
+        float deger = scanner.nextFloat();
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Matematik: ");
-        mat = input.nextInt();
-        System.out.print("Fizik: ");
-        fizik = input.nextInt();
-        System.out.print("Kimya: ");
-        kimya = input.nextInt();
-        System.out.print("Turkce: ");
-        turkce = input.nextInt();
-        System.out.print("Tarih: ");
-        tarih = input.nextInt();
-        System.out.print("Müzik: ");
-        muzik = input.nextInt();
-
-        int result = ortalama(mat, fizik, kimya, turkce, tarih, muzik);
-
-        System.out.println(result > 60 ? "Geçtiniz" : "Kaldınız");
+        float sonuc = kdvHesaplayici(deger);
+        System.out.println("Girilen Fiyat : " + deger + "\n" + "KDV'li fiyat : " + sonuc);
     }
 
-    private static int ortalama(int mat, int fizik, int kimya, int turkce, int tarih, int muzik) {
-        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
-        return toplam / 6;
+    private static float kdvHesaplayici(Float deger) {
+        float kdv;
+        int oran = (deger > 0 && deger <= 1000) ? 18 : 8;
+
+        kdv = (deger / 100) * oran;
+
+        return deger + kdv;
     }
 }
