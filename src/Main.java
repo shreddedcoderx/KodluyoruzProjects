b3d4734aa140d8100e744665d1958a7d39f54ff2
Adem <ademaldemir@hotmail.com.tr>
2022-07-08 23:16:55 +0300

    Dik Üçgende Hipotenüs VE ALAN Hesaplayıcı

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int mat, fizik, kimya, turkce, tarih, muzik;
+        int a, b;
+        double hipotenus, alan;
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Matematik: ");
-        mat = input.nextInt();
-        System.out.print("Fizik: ");
-        fizik = input.nextInt();
-        System.out.print("Kimya: ");
-        kimya = input.nextInt();
-        System.out.print("Turkce: ");
-        turkce = input.nextInt();
-        System.out.print("Tarih: ");
-        tarih = input.nextInt();
-        System.out.print("Müzik: ");
-        muzik = input.nextInt();
+        System.out.println("Üçgenin A kenarını girin:");
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
 
-        int result = ortalama(mat, fizik, kimya, turkce, tarih, muzik);
+        System.out.println("Üçgenin B kenarını girin:");
+        b = scanner.nextInt();
 
-        System.out.println(result > 60 ? "Geçtiniz" : "Kaldınız");
+        hipotenus = hipotenusHesapla(a, b);
+        System.out.println("Hipotenüs : " + hipotenus);
+
+        alan = ucgenAlan(a, b, hipotenus);
+        System.out.println("Üçgenin alanı : " + alan);
+
+        scanner.close();
     }
 
-    private static int ortalama(int mat, int fizik, int kimya, int turkce, int tarih, int muzik) {
-        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
-        return toplam / 6;
+    private static double ucgenAlan(int a, int b, double hipotenus) {
+        double cevre = ( a + b + hipotenus) / 2;
+
+        return Math.sqrt(cevre * (cevre - a) * (cevre - b) * (cevre - hipotenus));
+
+    }
+
+    private static double hipotenusHesapla(int a, int b) {
+        return Math.sqrt((a*a) + (b*b));
     }
 }
