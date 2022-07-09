656af2bf5bf3a4b1e84b571ea6629f792a398bf6
shreddedcoderx <shreddedcoderx@gmail.com>
2022-07-10 00:12:01 +0300

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,47 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5, total = 0;
+        int n1, n2, secim, sonuc;
+
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = scanner.nextInt();
 
-        System.out.println("Armut Kaç Kilo ? : ");
-        total += armut*scanner.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = scanner.nextInt();
 
-        System.out.println("Elma Kaç Kilo ? : ");
-        total += elma * scanner.nextInt();
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz: ");
+        secim = scanner.nextInt();
 
-        System.out.println("Domates Kaç Kilo ? : ");
-        total += domates * scanner.nextInt();
+        switch (secim) {
+            case 1:
+                sonuc = n1 + n2;
+                System.out.println("İşlemin sonucu : " + sonuc);
+                break;
+            case 2:
+                sonuc = n1 - n2;
+                System.out.println("İşlemin sonucu : " + sonuc);
+                break;
+            case 3:
+                sonuc = n1 * n2;
+                System.out.println("İşlemin sonucu : " + sonuc);
+                break;
+            case 4:
+                if (n2 != 0){
+                    sonuc = n1 / n2;
+                    System.out.println("İşlemin sonucu : " + sonuc);
+                    break;
+                } else {
+                    System.out.println("Bir sayı 0'a bölünemez.");
+                    break;
+                }
+            default:
+                System.out.println("Yanlış seçim yaptınız. Lütfen tekrar deneyiniz.");
 
-        System.out.println("Muz Kaç Kilo ? : ");
-        total += muz * scanner.nextInt();
+        }
 
-        System.out.println("Patlıcan Kaç Kilo ? : ");
-        total += patlican * scanner.nextInt();
-
-        System.out.println("Toplam Tutar : " + total);
     }
 
 }
