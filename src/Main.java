9871fb94e07020d9aa53839df90b8e745a2fcf93
shreddedcoderx <shreddedcoderx@gmail.com>
2022-07-09 03:17:47 +0300

    fiyatHesapla method deleted.
    Signed-off-by: shreddedcoderx <shreddedcoderx@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,16 @@ public class Main {
     public static void main(String[] args) {
         float mesafe;
         double perKm= 2.20, sonuc;
+        int acilis = 10;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Lütfen gideceğiniz KM'yi yazınız: " );
         mesafe = scanner.nextFloat();
 
-        sonuc = (mesafe*perKm) + 10 <= 20 ? 20.0 : fiyatHesapla(mesafe, perKm);
+        sonuc = (mesafe * perKm) + acilis;
+
+        sonuc = sonuc < 20 ? 20 : sonuc;
 
         System.out.println("Ödeyeceğiniz Tutar : " + sonuc);
-    }
-
-    private static double fiyatHesapla(float mesafe, double perKm) {
-        return 10 + (mesafe * perKm);
-
     }
 }
